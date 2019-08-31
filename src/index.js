0b49e1bead4c129f7793bd680e238a1aa1a238f9
Bozhidar Zahariev <bozhidar_zahariev@hotmail.com>
2019-08-31 11:02:08 +0100

    update

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import {
     createDom
 } from './createDom.js';
 
+import {
+    engine
+} from './engine.js';
 
 import './styles/reset.css';
 import css from './styles/style.css';
  
     
 
-
-    
-
 const content = document.getElementById('content');
 
   content.appendChild(createDom());
+
+ engine();
+  
